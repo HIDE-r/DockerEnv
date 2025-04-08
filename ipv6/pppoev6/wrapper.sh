6f525017eec66ba6958a730160239c8a0bb26d47
Collin L <lkangn.collin@gmail.com>
2025-04-08 11:13:10 +0800

    fix(pppoev6): fix environment variable can not pass to docker compose when privilege mode.

--- ipv6/pppoev6/wrapper.sh
+++ ipv6/pppoev6/wrapper.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
 
+verbose=0
 detach=0
 
 prt() {
-    echo "$(basename "$0"): $*"
+    if [ "$verbose" = "1" ]; then echo "$(basename "$0"): $*"; fi
+}
+
+run() {
+	if [ "${verbose}" = "1" ]; then
+		echo ""
+		echo "===> [RUN] $*"
+		echo ""
+	fi
+
+	eval "$*" || exit $?
 }
 
 usage() {
-    echo "usage: $(basename "$0") [-ilpds]"
+    echo "usage: $(basename "$0") [-ilpdsv]"
     echo "  options:"
     printf "  %-20s %s\n" "-i, --iface"         "The bind interface"
     printf "  %-20s %s\n" "-l, --local_ip"      "Local PPPoE interface IP"
     printf "  %-20s %s\n" "-p, --pool_start_ip" "Start address of remote IP pool"
     printf "  %-20s %s\n" "-d, --detach" 	"run docker compose in detach mode"
     printf "  %-20s %s\n" "-s, --super_user" 	"run docker compose in privilege"
+    printf "  %-20s %s\n" "-v, --verbose" 	"verbose message"
 }
 
 docker_compose_up() {
@@ -24,10 +36,15 @@ docker_compose_up() {
 		preface="sudo"
 	fi
 
+	# 当 docker compose 使用 sudo 调用 root 用户执行时, 当前的环境变量不起作用, 故这里使用变量替换的方式解决
+	preface+="IFACE=${IFACE} "
+	preface+="LOCAL_IP=${LOCAL_IP} "
+	preface+="POOL_START_IP=${POOL_START_IP} "
+
 	if [ "$detach" == "1" ]; then
-		${preface} docker compose up -d
+		run ${preface} docker compose up -d
 	else 
-		${preface} docker compose up
+		run ${preface} docker compose up
 	fi
 }
 
@@ -37,7 +54,7 @@ main() {
 		exit 0
 	fi
 
-	if ! OPTS=$(getopt -o 'hi:l:p:ds' --long help,iface:local_ip:pool_start_ip:detach,super_user -n 'parse-options' -- "$@"); then
+	if ! OPTS=$(getopt -o 'hi:l:p:dsv' --long help,iface:local_ip:pool_start_ip:detach,super_user,verbose -n 'parse-options' -- "$@"); then
 		err "Failed parsing options." >&2
 		usage
 		exit 1
@@ -53,6 +70,7 @@ main() {
 			-p | --pool_start_ip)	export POOL_START_IP="$2"; shift ; shift ;;
 			-d | --detach)		detach=1; shift;;
 			-s | --super_user)	privilege=1; shift;;
+			-v | --verbose)		verbose=1; shift;;
 			-- ) shift; break ;;
 			* ) err "unsupported argument $1"; usage; exit 1 ;;
 		esac
@@ -62,6 +80,7 @@ main() {
 	prt "local_ip=${LOCAL_IP}"
 	prt "pool_start_ip=${POOL_START_IP}"
 	prt "detach=${detach}"
+	prt "privilege=${privilege}"
 
 	echo -e "======================================================================\n"
 
